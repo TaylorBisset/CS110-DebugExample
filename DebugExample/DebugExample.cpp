5cd2efe8eb1b286852d4866822c98fb4b97baba5
Taylor Bisset <taylorbisset@gmail.com>
2023-04-20 10:52:31 -0700

    Initial commit; copied code from instructor

--- DebugExample/DebugExample.cpp
+++ DebugExample/DebugExample.cpp
@@ -1,20 +1,32 @@
-// DebugExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CreateVariables.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <cmath> //same as #include <math.h>
+
+using namespace std;
+
+void printTwoDecimals(double printMe)
+{
+    printf("%.2f \n", printMe);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Size of char: " << sizeof(char) << endl;
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of short int: " << sizeof(short int) << endl;
+    cout << "Size of long int: " << sizeof(long int) << endl;
+    cout << "Size of float: " << sizeof(float) << endl;
+    cout << "Size of double: " << sizeof(double) << endl;
+    cout << "Size of wchar_t: " << sizeof(wchar_t) << endl;
+
+    int firstNumber = 2.99999;
+    cout << firstNumber << endl;
+
+    double secondNumber = sqrt(firstNumber);
+    printTwoDecimals(secondNumber);
+
+    system("pause");
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
